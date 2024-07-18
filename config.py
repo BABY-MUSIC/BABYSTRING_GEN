9ee5d37627bacc6ed02e98361a881b306be9c7c3
BABY-MUSIC <tridevk442@gmail.com>
2024-07-18 17:57:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ID = int(getenv("API_ID","23342470"))
-API_HASH = getenv("API_HASH","824d7edf65f29a3bb7125173ff8ca9b0"))
+API_HASH = getenv("API_HASH","824d7edf65f29a3bb7125173ff8ca9b0")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID","6407861676"))
 
