10b2a7762a34c2496b97b9ad572059299a0edcde
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-11-19 13:37:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID","16457832"))
 API_HASH = getenv("API_HASH","3030874d0befdb5d05597deacc3e83ab")
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "7015056498:AAFaPW1VAfUH_xpEowOp5yacHOgu4y1mjoI")
 OWNER_ID = int(getenv("OWNER_ID","7400383704"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
